116c32d390da79240f2ff6d050340440f2dd193b
Forever Young <dy9040@gmail.com>
2022-03-10 23:00:40 -0600

    created a row query.

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ const db =mysql.createConnection(
     },
     console.log('Connected to the election database.')
 );
+db.query(`SELECT * FROM candidates`, (err, rows) =>{
+    console.log(rows);
+});
 
 //Defualt response for any other request (Not Found)
 app.use((req, res) => {
